cd5df77deffd324d30fe02f4064d3ff2952ef4ea
runarmyklebust <rmy@enonic.com>
2017-12-01 16:04:36 +0100

    Create ignite module (#5823)
    Set bundle-wide classloader

--- modules/core/core-ignite/src/main/java/com/enonic/xp/ignite/impl/IgniteActivator.java
+++ modules/core/core-ignite/src/main/java/com/enonic/xp/ignite/impl/IgniteActivator.java
@@ -6,11 +6,15 @@ import java.util.Map;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.Ignition;
 import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.osgi.classloaders.ContainerSweepClassLoader;
+import org.jetbrains.annotations.NotNull;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.enonic.xp.ignite.impl.config.IgniteConfig;
 
@@ -19,22 +23,28 @@ public class IgniteActivator
 {
     private ServiceRegistration<Ignite> igniteReg;
 
+    private Ignite ignite;
+
     private IgniteConfig config;
+
+    private final static Logger LOG = LoggerFactory.getLogger( IgniteActivator.class );
 
     @Activate
     public void activate( final BundleContext context, final Map<String, String> map )
     {
         final IgniteConfiguration igniteCfg = new IgniteConfiguration();
         igniteCfg.setDiscoverySpi( DiscoveryFactory.create( this.config ) );
+        igniteCfg.setClassLoader( createClassLoader( context ) );
 
-        //final Ignite ignite = Ignition.start();
-        Ignite ignite = Ignition.start( igniteCfg );
-
-        System.out.println( " -----------------------------------------------------------------" );
-        System.out.println( "------Setting ignite object (ACTIVATOR): " + ignite.hashCode() );
-        System.out.println( " -----------------------------------------------------------------" );
+        this.ignite = Ignition.start( igniteCfg );
 
         this.igniteReg = context.registerService( Ignite.class, ignite, new Hashtable<>() );
+    }
+
+    @NotNull
+    private ContainerSweepClassLoader createClassLoader( final BundleContext context )
+    {
+        return new ContainerSweepClassLoader( context.getBundle(), this.getClass().getClassLoader() );
     }
 
     @Reference
